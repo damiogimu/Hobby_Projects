015929bb00f7cdf3c41d7129d6a15644fe03a27b
daimu-mogi <60080040+damiogimu@users.noreply.github.com>
2023-03-03 16:35:46 +0900

    add maez.h

---
+++ maze.h
@@ -0,0 +1,28 @@
+#ifndef MAZE_H
+# define MAZE_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define PLAYER	'0'
+#define LAND	'1'
+#define WALL	'2'
+#define PASSED	'3'
+#define GOAL	'4'
+#define NOW		'5'
+
+#define MAX_WIDTH 100
+#define MAX_HEIGHT 100
+
+#define WIDTH 40
+#define HEIGHT 40
+
+#define OUT_F "map.txt"
+
+#endif
